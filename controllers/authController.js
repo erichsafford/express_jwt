15a714c8f0198b31a967580a346d136b9f856117
Erich Gaul <erichgaul@gmail.com>
2023-10-04 10:57:17 -0500

    Complete chapter 12 tut

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,13 +13,19 @@ const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
-    if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    // evaluate password 
+    if (!foundUser) return res.sendStatus(401); //Unauthorized
+    // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
+        const roles = Object.values(foundUser.roles)
         // create JWTs
         const accessToken = jwt.sign(
-            { "username": foundUser.username },
+            { UserInfo:
+                {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
@@ -36,7 +42,7 @@ const handleLogin = async (req, res) => {
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(usersDB.users)
         );
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 }); // remove secure prop when testing refresh/cookies
         res.json({ accessToken });
     } else {
         res.sendStatus(401);
